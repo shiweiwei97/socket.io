8d666809395b12584bac3f16b3e5cd6c73a2b09b
Weiwei Shi <shiweiwei97@gmail.com>
2018-09-16 16:21:29 +0800

    simple chat example

---
+++ examples/simple-chat/index.js
@@ -0,0 +1,33 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('../..')(server);
+var port = process.env.PORT || 3000;
+
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.broadcast.emit('hi');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+    socket.broadcast.emit('good bye');
+  });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
